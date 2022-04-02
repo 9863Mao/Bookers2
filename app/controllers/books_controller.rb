6a4cb72345f5ec7c728e59a6d6ea54065c307ae9
9863mao <mao63black@gmail.com>
2022-04-02 13:08:56 +0000

    save

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,22 +1,37 @@
 class BooksController < ApplicationController
   def index
-    @books = Book.all
+    @book = Book.new
+    @books = Book.page(params[:page])
+    @user = current_user
   end
   def new
-    @book = Book.new
   end
   def create
+    @books = Book.all
+    @user = current_user
     @book = Book.new(book_params)
     @book.user_id = current_user.id
-    @book.save
-    redirect_to books_path
+    if @book.save
+      redirect_to books_path
+    else
+      render :index
+    end
   end
   def show
+    @book = Book.find(params[:id])
+    @user = @book.user
+    @books = Book.all
+  end
+
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to books_path
   end
 
   private
 
   def book_params
-    params.require(:book).permit(:book_title, :caption)
+    params.require(:book).permit(:title, :caption)
   end
 end
